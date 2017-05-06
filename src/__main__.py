a986deed82d4654717d2e3d871f28089b717fb2a
Andreas Lindeboom <a.s.lindeboom@gmail.com>
2017-05-06 21:26:38 +0200

    Fix imports

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,5 @@
 import docker
-import support
+from support import *
 
 from roletester import RoleTester
 
@@ -7,16 +7,16 @@ def main():
     config_path = "testconfig.yml"
     container_network_id = 'role_tester_network'
 
-    container_manager = support.ContainerManager(
+    container_manager = ContainerManager(
         docker_client = docker.from_env(),
         container_network_id = container_network_id)
 
     role_tester = RoleTester(
-        support.KeyGenerator(container_manager),
-        support.AnsibleManager(container_manager),
-        support.TargetManager(container_manager))
+        KeyGenerator(container_manager),
+        AnsibleManager(container_manager),
+        TargetManager(container_manager))
 
-    test_config = support.YamlFileLoader.load_yaml(config_path)
+    test_config = YamlFileLoader.load_yaml(config_path)
 
     role_tester.test_roles(test_config)
 
