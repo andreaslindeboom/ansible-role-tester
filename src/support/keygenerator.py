41733e1e20654f0262bbbf819a1559b3a6d02db2
Andreas Lindeboom <a.s.lindeboom@gmail.com>
2017-05-07 17:30:21 +0200

    Keypair cleanup

--- src/support/keygenerator.py
+++ src/support/keygenerator.py
@@ -1,8 +1,14 @@
+import os
+
 class KeyGenerator:
     keygen_version = "0.1.1"
 
     def __init__(self, container_manager):
         self.container_manager = container_manager
+
+    def __del__(self):
+        print("\n--- Key cleanup ---")
+        self._cleanup()
 
     def generate_keypair(self, filename):
         print("Generating keypair to enable Ansible to access target over SSH")
@@ -10,4 +16,12 @@ class KeyGenerator:
             'lindeboomio/openssh-keygen-alpine:{}'.format(self.keygen_version),
             volumes = dict([('keys', '/keys')]),
             command = "-t rsa -b 2048 -P '' -f {}".format(filename))
+
+    def _cleanup(self):
+        key_files = os.listdir("/keys")
+        for key in key_files:
+            file_path = "/keys/{}".format(key)
+            if os.path.isfile(file_path):
+                print("Cleaning up {}".format(file_path))
+                os.unlink(file_path)
 
