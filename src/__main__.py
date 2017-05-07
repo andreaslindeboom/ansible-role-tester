efd26f1f68eaa1b48c596a2807fbb72aafa14916
Andreas Lindeboom <a.s.lindeboom@gmail.com>
2017-05-07 23:29:41 +0200

    Simpler turning off cleanup

--- src/__main__.py
+++ src/__main__.py
@@ -7,18 +7,19 @@ def main():
     config_path = "testconfig.yml"
     container_network_id = 'role_tester_network'
     key_name = 'ansible'
+    cleanup = False
 
     print("--- General preparation ---")
 
     container_manager = ContainerManager(
         docker_client = docker.from_env(),
         container_network_id = container_network_id,
-        cleanup = False)
+        cleanup = cleanup)
 
     key_manager = KeyManager(
         container_manager,
         key_name,
-        cleanup = False)
+        cleanup = cleanup)
 
     role_tester = RoleTester(
         key_manager,
